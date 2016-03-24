7ac9dbfa28bff4dda634cdba51d317a7e8f815c9
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2016-03-24 11:04:33 -0400

    Users can now log in

--- client/app/signIn/signIn.controller.js
+++ client/app/signIn/signIn.controller.js
@@ -1,18 +1,18 @@
 'use strict';
 
 angular.module('tp1FullstackApp')
-	.controller('SignInCtrl', function ($scope, $http) {
+	.controller('SignInCtrl', function ($scope, $http, $location) {
 		
 		$scope.sendForm= function(){
+			var url= 'https://crispesh.herokuapp.com/api/login_check';
+			var data= {username: $scope.signIn.email, password: $scope.signIn.password};
 			$http({
 				method: 'POST',
-				url: 'https://crispesh.herokuapp.com/api/login_check',
-				data: {
-					'email': $scope.signIn.email,
-					'password': $scope.signIn.password
-				}
+				url: url,
+				data: data,
 			}).then(function successCallback(response) {
 				$location.path('/');
+				$scope.loggedIn=true;
 			}, function errorCallback(response) {
 				console.log(response);
 				$scope.invalidLogin=true;
