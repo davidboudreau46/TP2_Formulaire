eab432a00d373b48fb86dc8272f8bbfc85563fdb
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2016-03-31 11:38:19 -0400

    Revert "Revert "Playlist""
    This reverts commit 61d26f972909af00e0374c1df6722422b12e5918.

--- client/app/playlist/playlist.js
+++ client/app/playlist/playlist.js
@@ -4,6 +4,7 @@ angular.module('tp1FullstackApp')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/playlist', {
-        template: '<playlist></playlist>'
+        templateUrl: 'app/playlist/playlist.html',
+        controller: 'PlaylistCtrl'
       });
   });
