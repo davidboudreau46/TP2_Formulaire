c3d4ad796508b194827129f9e5f10d05fdf81625
David <divad95@live.fr>
2016-04-07 15:50:24 -0400

    Tests Playlist

--- client/app/contact/contact.controller.spec.js
+++ client/app/contact/contact.controller.spec.js
@@ -18,36 +18,32 @@ describe('Controller: ContactCtrl', function () {
 			return $controller('ContactCtrl', {'$scope' : $rootScope });
 		};
 	}));
-	
+
 	it('with invalid informations should show errors', function () {
 		var controller = createController();
 		$httpBackend.expectPOST('https://crispesh.herokuapp.com/api/contact').respond(500, '');
-		$httpBackend.when('GET', 'app/main/main.html').respond('');
-		$httpBackend.expectGET('app/main/main.html');
-		
+
 		$rootScope.email='thisIsNotAValidEmail';
 		$rootScope.reason='test';
 		$rootScope.message='lol';
 		$rootScope.name='bob';
 		$rootScope.sendForm();
 		$httpBackend.flush();
-		
+
 		expect($rootScope.invalidMessage).toBe(true);
 	});
-	
+
 	it('with valid informations should show not errors', function () {
 		var controller = createController();
 		$httpBackend.expectPOST('https://crispesh.herokuapp.com/api/contact').respond(201, '');
-		$httpBackend.when('GET', 'app/main/main.html').respond('');
-		$httpBackend.expectGET('app/main/main.html');
-		
+
 		$rootScope.email='test@test.test';
 		$rootScope.reason='test';
 		$rootScope.message='lol';
 		$rootScope.name='bob';
 		$rootScope.sendForm();
 		$httpBackend.flush();
-		
+
 		expect($rootScope.sentForm).toBe(true);
 	});
 
--- e2e/contact/contact.js
+++ e2e/contact/contact.js
@@ -1,60 +1,60 @@
 describe('Controller: ContactCtrl', function () {
-	
+
 	it('success message should not be shown from start', function() {
 		browser.get('#/contact');
-		
+
 		element(by.model('form')).isDisplayed().then(function(visible){
 			expect(visible).toBeTruthy();
 		});
-		
+
 		element(by.id('successMessage')).isDisplayed().then(function(visible) {
 			expect(visible).toBeFalsy();
 		});
 	});
-	
+
 	it('invalid email should show message', function() {
 		browser.get('#/contact');
-		
+
 		element(by.model('email')).sendKeys('IamNotAnEmail');
-		
+
 		element(by.model('name')).click();
-		
+
 		element(by.id('invalidEmailError')).isDisplayed().then(function(visible){
 			expect(visible).toBeTruthy();
 		});
 	});
-	
+
 	it('empty email should show message', function() {
 		browser.get('#/contact');
-		
+
 		element(by.model('email')).sendKeys(1);
-		
+
 		element(by.model('email')).clear();
-		
+
 		element(by.id('emptyEmailError')).isDisplayed().then(function(visible){
 			expect(visible).toBeTruthy();
 		});
 	});
-	
+
 	it('empty user name should show error', function() {
 		browser.get('#/contact');
-		
+
 		element(by.model('name')).sendKeys(1);
-		
+
 		element(by.model('name')).clear();
-		
+
 		element(by.id('emptyNameError')).isDisplayed().then(function(visible){
 			expect(visible).toBeTruthy();
 		});
 	});
-	
+
 	it('empty user name should show error', function() {
 		browser.get('#/contact');
-		
+
 		element(by.model('message')).sendKeys(1);
-		
+
 		element(by.model('message')).clear();
-		
+
 		element(by.id('emptyMessageError')).isDisplayed().then(function(visible){
 			expect(visible).toBeTruthy();
 		});
